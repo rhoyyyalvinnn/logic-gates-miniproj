639a61adea4468578518e178e78d8fced98e1522
rhoyyyalvinnn <94331000+rhoyyyalvinnn@users.noreply.github.com>
2025-05-05 11:14:52 +0800

    ajksdffdsss

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -26,4 +27,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
